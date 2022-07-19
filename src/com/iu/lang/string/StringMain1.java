a6fdb0dcd77303c4cb0c96f7334c8bfb824761e3
kimdaeki <kimdaeki@gmail.com>
2022-07-19 14:04:18 +0900

    string2

--- src/com/iu/lang/string/StringMain1.java
+++ src/com/iu/lang/string/StringMain1.java
@@ -23,7 +23,9 @@ public class StringMain1 {
 		//s2.studyIndexOfEx1();
 		//s2.studyIndexOf();
 		//s2.studySubString();
-		s2.ex2();
+//		s2.ex2();
+		StringStudy3 s3 = new StringStudy3();
+		s3.studySplit3();
 		
 		
 
