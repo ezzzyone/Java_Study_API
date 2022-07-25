0250f1525be8cda4b5b4d94f70fc3af42d5db22e
jiwon22lee <jiwon1799@gmail.com>
2022-07-25 17:47:13 +0900

    thread

--- src/com/iu/thread/ThreadMain.java
+++ src/com/iu/thread/ThreadMain.java
@@ -9,9 +9,11 @@ public class ThreadMain {
 		Thread t = new Thread(t2); //t를 실행하면 t2에 있는 run이 실행됨.
 		
 		//thread 사용시, 개발자가 직접 호출하지 않고, os에 실행부탁함(start로 실행)
-		t1.start();
-		t2.start();
+//		t1.start();
+//		t2.start();
 		//thread 사용하여 번갈아가며 실행됨.
+		
+		t.start();
 		
 		
 		
